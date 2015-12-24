4ea8e14d6b542d5b0901ae46fe01e4eb082f8238
xuncl <xuncl@126.com>
2015-12-24 20:40:31 +0800

    Add loading and whatsnew activity

--- app/src/main/java/com/cjkzy/cjkzy/supermine/LoadingActivity.java
+++ app/src/main/java/com/cjkzy/cjkzy/supermine/LoadingActivity.java
@@ -1,9 +1,27 @@
 package com.cjkzy.cjkzy.supermine;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
 
 /**
- * Created by CLEVO on 2015/12/21.
+ * Created by xuncl on 2015/12/21.
  */
-public class LoadingActivity extends Activity{
+public class LoadingActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loading);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(LoadingActivity.this, Whatsnew.class);
+                startActivity(intent);
+                LoadingActivity.this.finish();
+                Toast.makeText(getApplicationContext(), "登录成功", Toast.LENGTH_SHORT).show();
+            }
+        }, 200);
+    }
 }
---
+++ app/src/main/java/com/cjkzy/cjkzy/supermine/Whatsnew.java
@@ -0,0 +1,10 @@
+package com.cjkzy.cjkzy.supermine;
+
+import android.app.Activity;
+
+/**
+ * Created by CLEVO on 2015/12/22.
+ */
+public class Whatsnew extends Activity{
+
+}
